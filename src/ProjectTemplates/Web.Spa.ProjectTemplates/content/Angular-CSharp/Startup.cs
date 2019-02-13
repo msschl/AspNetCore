25e7fcc347c4cd06b20ba2778ee5137735a4a70f
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-02-13 07:04:05 -0800

    Templatize content

--- src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.Spa.ProjectTemplates/content/Angular-CSharp/Startup.cs
@@ -1,10 +1,22 @@
+#if (IndividualLocalAuth)
+using Microsoft.AspNetCore.Authentication;
+#endif
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-#if (!NoHttps)
+#if (IndividualLocalAuth)
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+#endif
+#if (RequiresHttps)
 using Microsoft.AspNetCore.HttpsPolicy;
 #endif
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+#if (IndividualLocalAuth)
+using Microsoft.EntityFrameworkCore;
+using Company.WebApplication1.Data;
+using Company.WebApplication1.Models;
+#endif
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,27 +34,26 @@ namespace Company.WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(
-                o => o.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+#if (IndividualLocalAuth)
+            services.AddDbContext<ApplicationDbContext>(options =>
+    #if (UseLocalDB)
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+    #else
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")));
+    #endif
 
             services.AddDefaultIdentity<ApplicationUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(
-                    ao => ao.Clients.AddIdentityServerSPA(
-                        "angular",
-                        spa =>
-                        {
-                            spa
-                            .WithRedirectUri("/authentication/login-callback")
-                            .WithLogoutRedirectUri("/authentication/logout-callback");
-                        }));
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
 
             services.AddAuthentication()
                 .AddIdentityServerJwt();
-
+#endif
             services.AddMvc()
                 .AddNewtonsoftJson();
 
@@ -59,11 +70,14 @@ namespace Company.WebApplication1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+#if (IndividualLocalAuth)
+                app.UseDatabaseErrorPage();
+#endif
             }
             else
             {
                 app.UseExceptionHandler("/Error");
-#if (!NoHttps)
+#if (RequiresHttps)
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -75,6 +89,11 @@ namespace Company.WebApplication1
 #endif
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+#if (IndividualLocalAuth)
+
+            app.UseAuthentication();
+            app.UseIdentityServer();
+#endif
 
             app.UseMvc(routes =>
             {
